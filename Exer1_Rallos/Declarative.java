0f5df8de5a5b753e3c8b112cc098863b720d4bc6
2024-1976 <jareltrallos93@gmail.com>
2025-09-09 14:25:33 +0800

    Declarative.java

---
+++ Exer1_Rallos/Declarative.java
@@ -0,0 +1,27 @@
+//exert1_Declarative.java
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DeclarativeExample {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+       
+        int sumOfSquares = numbers.stream()
+                                  .filter(n -> n % 2 == 0)   
+                                  .map(n -> n * n)           
+                                  .reduce(0, Integer::sum);    
+        System.out.println("Sum of squares of even numbers: " + sumOfSquares);
+
+     
+        List<String> words = Arrays.asList("hello", "world");
+
+        String concatenatedUpperCase = words.stream()
+                                           .map(String::toUpperCase) 
+                                           .reduce("", (a, b) -> a + b);
+
+        System.out.println("Concatenated uppercase: " + concatenatedUpperCase); 
+    }
+}
